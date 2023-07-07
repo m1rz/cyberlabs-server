2ac10aaa02387414c8d985c6f27bba48bc91ecbd
jcermat <22714237+jcermat@users.noreply.github.com>
2023-07-07 08:41:40 +0800

    changed init.py again

--- __init__.py
+++ __init__.py
@@ -16,7 +16,7 @@ app.config['DATABASE_CONN_STRING'] = "mongodb://root:cyberlabs@localhost:27017/"
 app.config['DATABASE_NAME'] = "cyberlabs"
 cors = CORS(app)
 jwt = JWTManager(app)
-sockio = SocketIO(app, cors_allowed_origins="cyberlabs.surge.sh")
+sockio = SocketIO(app, origins=["https://cyberlabs.surge.sh"])
 
 from auth import auth
 from admin import admin
