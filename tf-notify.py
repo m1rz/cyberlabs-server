19f42b619bec2f803a2db7843f9dd82fe5dee974
jcermat <22714237+jcermat@users.noreply.github.com>
2023-07-24 15:08:22 +0800

    updated tf-notify to right path

--- tf-notify.py
+++ tf-notify.py
@@ -5,8 +5,11 @@ import subprocess
 def monitor():
     i = inotify.adapters.Inotify()
     watch_mask = inotify.constants.IN_CLOSE_WRITE | inotify.constants.IN_DELETE
-    i.add_watch('config', watch_mask)
+    i.add_watch('config/proxmox', watch_mask)
 
     for event in i.event_gen(yield_nones=False):
         subprocess.run(['TF_IN_AUTOMATION=true','terraform', 'apply', '-auto-approve', '-input=false'])
+
+if __name__ == '__main__':
+    monitor()
 
